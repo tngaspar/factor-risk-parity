c40c69e5132248f52d882ff8013230127d574ea0
Tiago Gaspar <43268598+tngaspar@users.noreply.github.com>
2020-05-01 21:33:59 +0200

    portfolio rp applied in main

--- main.py
+++ main.py
@@ -6,32 +6,41 @@ import riskparityportfolio as rp
 import datetime as dt
 # from backtester import portfolio_construction
 import stock_data
+from dateutil.relativedelta import relativedelta
+from alive_progress import alive_bar
+from alive_progress import config_handler
+config_handler.set_global(force_tty=True)
 
 # Import portfolio construction methods
-import risk_parity
+from risk_parity import weights_risk_parity
 import factor_risk_parity
 
 # Data
-tickers_t = ['GOOGL', 'AAPL', 'AMZN']
-start_date_t = dt.datetime(2015, 10, 31)
-end_date_t = dt.datetime(2017, 10, 31)
+tickers = ['GOOGL', 'AAPL', 'AMZN']
+start_date = dt.datetime(2015, 10, 31)
+end_date = dt.datetime(2017, 10, 31)
 
 # Running methods
-# w_rp = weights_risk_parity(end_date, start_date, tickers)
+# w_rp = weights_risk_parity(tickers, start_date, end_date)
 # print(w_rp)
 
 # testing area:
 
 
-#def portfolio_construction(method, tickers, start_date, end_date, update_period):
-tickers = tickers_t
-start_date = start_date_t
-end_date = end_date_t
+portfolio_update_frequency = 'BM'
+business_days_end_months = pd.date_range(start_date, end_date, freq='BM')
+#w = weights_risk_parity(tickers, business_days_end_months[0]+ relativedelta(years=-1), business_days_end_months[0])
+portfolio_weights = pd.DataFrame(index=business_days_end_months, columns=tickers)
 
-prices = stock_data.get_prices(tickers, start_date, end_date)
+with alive_bar(len(business_days_end_months)) as bar:
+    for t in business_days_end_months:
+        portfolio_weights.loc[t] = weights_risk_parity(tickers, t + relativedelta(months=-3), t)
+        bar()
+
+#prices = stock_data.get_prices(tickers, start_date, end_date)
 #portfolio weights for last business day of month
-portfolio = pd.DataFrame([prices.resample('BM', convention='end').asfreq().index], index=['Date']).T
-portfolio['weights'] = portfolio['Date'].apply(risk_parity.weights_risk_parity, axis='columns', raw=False, result_type='expand', args=(end_date-dt.timedelta(days=365), tickers))
+#portfolio = pd.DataFrame([prices.resample('BM', convention='end').asfreq().index], index=['Date']).T
+#portfolio['weights'] = portfolio['Date'].apply(risk_parity.weights_risk_parity, axis='columns', raw=False, result_type='expand', args=(end_date-dt.timedelta(days=365), tickers))
 
 
 
--- risk_parity.py
+++ risk_parity.py
@@ -6,7 +6,7 @@ import datetime
 import stock_data
 
 
-def weights_risk_parity(end_date, start_date, tickers):
+def weights_risk_parity(tickers, start_date, end_date):
     prices = stock_data.get_prices(tickers, start_date, end_date)
     cov_matrix = stock_data.get_covariance_matrix(prices)
     budget = np.full(len(tickers), 1 / len(tickers))  # parity of risk budget
@@ -15,3 +15,9 @@ def weights_risk_parity(end_date, start_date, tickers):
     return w
 
 
+def portfolio_weights_risk_parity(tickers, start_date, end_date, portfolio_update_period):
+
+    #bussiness_days_rng = pd.date_range(start, end, freq='BM')
+    w=1
+
+    return w
